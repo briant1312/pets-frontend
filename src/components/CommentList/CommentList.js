43ad95c66f52b3698babfd215839f086ffe6013f
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-06-25 12:06:35 -0400

    remove incorrect import statement

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,12 +1,10 @@
-import CommentListItem from "../CommentListItem/CommentListItem.js/index.js"
+import CommentListItem from "../CommentListItem/CommentListItem.js"
 
 export default function CommentList({comments}){
     return (
         <>
         {comments && comments.map((comment)=>(<CommentListItem key={comment._id} comment={comment}/>)) }
-        <p>hello</p>
         </>
     )
 }
 
-// comment
--- src/components/CommentListItem/CommentListItem.js
+++ src/components/CommentListItem/CommentListItem.js
@@ -3,7 +3,6 @@ export default function CommentListItem({comment}){
         <div>
             <p>{comment.text}</p>
             <p>By: {comment.owner.userName}</p>
-            <p>hello</p>
         </div>
     )
 }
