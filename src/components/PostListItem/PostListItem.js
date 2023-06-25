f9eb1974c6b0389e6f0ff4e2c4c28518ca38735b
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-06-25 12:58:42 -0400

    add functionality for liking and disliking and commenting on posts

--- src/components/PostListItem/PostListItem.js
+++ src/components/PostListItem/PostListItem.js
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 // import { TbArrowBigUpFilled, TbArrowBigDownFilled } from "react-icons/tb"
-import { useParams } from "react-router-dom"
+import { likePost } from "../../utilities/post-api"
+import { dislikePost } from "../../utilities/post-api"
+
 
 export default function PostListItem({ post, user }) {
-    // const [likeTotal, setLikeTotal] = useState(post.likes.length - post.dislikes.length)
+    const [likeTotal, setLikeTotal] = useState(post.likes.length - post.dislikes.length)
     // const [userLiked, setUserLiked] = useState(post.likes.includes(user._id));
     // const [userDisliked, setUserDisliked] = useState(post.dislikes.includes(user._id));
     const navigate = useNavigate()
@@ -13,11 +15,29 @@ export default function PostListItem({ post, user }) {
         navigate(`/show/${post._id}`)
     }
 
+    const handleLike = async (e) => {
+        e.stopPropagation()
+        const updatedPost = await likePost(post._id)
+        setLikeTotal(updatedPost.likes.length - updatedPost.dislikes.length)
+    }
+
+    const handleDislike = async (e) => {
+        e.stopPropagation()
+        const updatedPost = await dislikePost(post._id)
+        setLikeTotal(updatedPost.likes.length - updatedPost.dislikes.length)
+    }
+    
     return (
         <div onClick={handleShowPost}>
             <h2>{post.title}</h2>
             <p>{post.text}</p>
             <p>{post.animal}</p>
+            <div>
+                <button onClick={handleLike}>Like</button>
+                {likeTotal}
+                <button onClick={handleDislike}>Disike</button>
+            </div>
+            <p>comments: {post.comments.length}</p>
         </div>
     )
 }
