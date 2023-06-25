2a23d78e7361ef838b818eeea3acd2590b03c9a0
Greg Harmon <gregharmon@Gregs-MacBook-Pro.local>
2023-06-25 09:45:14 -0600

    added comment components and mapping

---
+++ src/components/CommentList/commentList.js
@@ -0,0 +1,9 @@
+import CommentListItem from "../CommentListItem/CommentListItem.js"
+
+export default function CommentList({comments}){
+    return (
+        <>
+        {comments && comments.map((comment)=>(<CommentListItem key={comment._id} comment={comment}/>)) }
+        </>
+    )
+}
---
+++ src/components/CommentListItem/commentListItem.js
@@ -0,0 +1,8 @@
+export default function CommentListItem({comment}){
+    return (
+        <div>
+            <p>{comment.text}</p>
+            <p>By: {comment.owner.userName}</p>
+        </div>
+    )
+}
